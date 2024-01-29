3d708f59cca832424dca60a9b106de19c1edbcb0
jhk742 <jhk742@nyu.edu>
2024-01-29 18:44:40 +0900

    included a helpers folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import Square from './Square'
-import { Patterns } from './Patterns'
+import { Patterns } from '../helpers/Patterns'
 
 export default function App() {
 
